5e92054457c022036df074a1cef75dbc97998967
angel <angel0308dl@naver.com>
2025-04-24 20:29:43 +0900

    first commit

---
+++ src/main/java/jungyeon/b_side/BSideApplication.java
@@ -0,0 +1,13 @@
+package jungyeon.b_side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BSideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jungyeon/b_side/BSideApplicationTests.java
@@ -0,0 +1,13 @@
+package jungyeon.b_side;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
